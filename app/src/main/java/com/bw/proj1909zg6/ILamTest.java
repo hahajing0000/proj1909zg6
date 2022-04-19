de0fd517c5da5d710a53c0d552e5b0b80ccdc02e
hahajing <444511958@qq.com>
2022-04-19 11:11:23 +0800

    完成数据绑定Demo 单向 双向 按钮事件处理
    Signed-off-by: hahajing <444511958@qq.com>

---
+++ app/src/main/java/com/bw/proj1909zg6/ILamTest.java
@@ -0,0 +1,17 @@
+package com.bw.proj1909zg6;
+
+import android.view.View;
+
+/**
+ * @author hahajing 企鹅：444511958
+ * @version 1.0.0
+ * @createDate 2022/4/19 11:03
+ * @description
+ * @updateUser hahajing
+ * @updateDate 2022/4/19 11:03
+ * @updateRemark
+ */
+@FunctionalInterface
+public interface ILamTest {
+    void onClick(View view);
+}
--- app/src/main/java/com/bw/proj1909zg6/MainActivity.java
+++ app/src/main/java/com/bw/proj1909zg6/MainActivity.java
@@ -2,6 +2,7 @@ package com.bw.proj1909zg6;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 import androidx.databinding.Observable;
 import androidx.databinding.ObservableField;
 import androidx.databinding.ObservableInt;
@@ -9,12 +10,18 @@ import androidx.databinding.ObservableInt;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.bw.proj1909zg6.databinding.ActivityMainBinding;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+//    private Button btnTest;
 
     public ObservableField<String> testTxt=new ObservableField<>();
 //    ObservableInt observableInt=new ObservableInt();
@@ -25,35 +32,45 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding= DataBindingUtil.setContentView(this,R.layout.activity_main);
+        binding.setOwn(this);
 
-        ArrayList<String> list=new ArrayList<>();
-        list.add("1");
-        list.add("10");
-//        list.add(100.0f);
-//        list.add(1000L);
+//        btnTest = (Button) findViewById(R.id.btn_test);
+//        btnTest.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//            }
+//        });
 
-        Class<? extends ArrayList> clazz = list.getClass();
-        Method[] methods = clazz.getDeclaredMethods();
-        for (Method method:methods){
-            if (method.getName().equals("add")){
-                try {
-                    if (method.getGenericParameterTypes().length==1){
-                        method.invoke(list,10);
-                        method.invoke(list,100.0F);
-                        method.invoke(list,1000L);
-                    }
-
-
-                } catch (Exception exception) {
-                    exception.printStackTrace();
-                }
-            }
-        }
-
-        for (Object obj:list){
-            Log.d("123", "onCreate: "+obj);
-        }
+//        ArrayList<String> list=new ArrayList<>();
+//        list.add("1");
+//        list.add("10");
+////        list.add(100.0f);
+////        list.add(1000L);
+//
+//        Class<? extends ArrayList> clazz = list.getClass();
+//        Method[] methods = clazz.getDeclaredMethods();
+//        for (Method method:methods){
+//            if (method.getName().equals("add")){
+//                try {
+//                    if (method.getGenericParameterTypes().length==1){
+//                        method.invoke(list,10);
+//                        method.invoke(list,100.0F);
+//                        method.invoke(list,1000L);
+//                    }
+//
+//
+//                } catch (Exception exception) {
+//                    exception.printStackTrace();
+//                }
+//            }
+//        }
+//
+//        for (Object obj:list){
+//            Log.d("123", "onCreate: "+obj);
+//        }
 
 //        observableInt.set(10);
 //
@@ -64,4 +81,22 @@ public class MainActivity extends AppCompatActivity {
 //            }
 //        });
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        testTxt.set("我是小明同学");
+    }
+
+    public void onClick(View view){
+//        Toast.makeText(this, "111", Toast.LENGTH_SHORT).show();
+        String s = testTxt.get();
+        Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
+    }
+
 }
